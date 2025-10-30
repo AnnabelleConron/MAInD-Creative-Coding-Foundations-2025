14bd266a0c7d39cc315e09aeadbf6b83a5a83e27
Annabelle Conron <annabelle.conron@outlook.com>
2025-10-30 16:11:40 +0100

    Create new list element with JS

--- Projects/Project-01/Project-01.3/assets/script.js
+++ Projects/Project-01/Project-01.3/assets/script.js
@@ -6,5 +6,15 @@ const taskInput = document.getElementById('todo-input');
 // Event listener to the add button using the addButton and taskInput reference
 // Listener for the add button
 addButton.addEventListener('click', () => {
+    // detecting a click on the add button
     console.log('Add button clicked');
+
+    const inputValue = taskInput.value;
+    // can reuse in to check with the console that the input value is being detected when the add button is clicked
+    console.log(inputValue);
+
+    const listElement = document.createElement('li');
+    // create new list element
+    // get inputValue and use it as the text content for the new list element
+    listElement.innerHTML = inputValue;
 })
