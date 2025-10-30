a8de980962e9d9fcd4524dcec361f563e6930e05
Annabelle Conron <annabelle.conron@outlook.com>
2025-10-30 16:19:26 +0100

    Add list view and card view button to JS

--- Projects/Project-01/Project-01.3/assets/script.js
+++ Projects/Project-01/Project-01.3/assets/script.js
@@ -1,6 +1,9 @@
 // Get references to the input field and add button
 const addButton = document.getElementById('add-button');
 
+const listViewButton = document.getElementById('list-view-button');
+const cardViewButton = document.getElementById('card-view-button');
+
 const taskInput = document.getElementById('todo-input');
 
 // Event listener to the add button using the addButton and taskInput reference
@@ -17,6 +20,13 @@ addButton.addEventListener('click', () => {
     // create new list element
     listElement.innerHTML = inputValue;
     // get inputValue and use it as the text content for the new list element
+
+    listElement.classList.add('todo-item');
+    // add the class "todo-item" to the new list element
+
     document.getElementById('todo-list').appendChild(listElement);
     // append the new list element to the existing todo-list ul element
+
+    taskInput.value = '';
+    // clear the input field after adding the item
 })
