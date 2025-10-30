8106a46632dc5321170d6957e33fe05503e72a66
Annabelle Conron <annabelle.conron@outlook.com>
2025-10-30 16:00:24 +0100

    Add event listener to Add Button

---
+++ Projects/Project-01/Project-01.3/assets/script.js
@@ -0,0 +1,10 @@
+// Get references to the input field and add button
+const addButton = document.getElementById('add-button');
+
+const taskInput = document.getElementById('todo-input');
+
+// Event listener to the add button using the addButton and taskInput reference
+// Listener for the add button
+addButton.addEventListener('click', () => {
+    console.log('Add button clicked');
+})
