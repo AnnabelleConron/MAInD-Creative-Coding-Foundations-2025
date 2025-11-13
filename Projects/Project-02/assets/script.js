72a6c1ab099433ceb5434a5349a98fc1a3fb0d47
Annabelle Conron <annabelle.conron@outlook.com>
2025-11-13 21:01:15 +0100

    Create shuffle function

--- Projects/Project-02/assets/script.js
+++ Projects/Project-02/assets/script.js
@@ -12,3 +12,11 @@ let matchedCards = [];
 const iconColors = ['#e74c3c', '#f1c40f', '#3498db', '#9b59b6', '#2ecc71', '#e67e22', '#1abc9c', '#ff4757'];
 
 resetBtn.addEventListener("click", resetGame);
+
+// Shuffle function
+function shuffle(array) {
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i+1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
