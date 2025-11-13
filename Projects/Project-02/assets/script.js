8ee8bde25762ee2fad3f016a1eff311b64332abd
Annabelle Conron <annabelle.conron@outlook.com>
2025-11-13 20:55:29 +0100

    Add icon colours in an array

--- Projects/Project-02/assets/script.js
+++ Projects/Project-02/assets/script.js
@@ -7,3 +7,8 @@ const board = document.getElementById("game-board");
 const status = ocument.getElementById("status");
 const resetBtn = ocument.getElementById("reset-btn");
 
+let flippedCards = [];
+let matchedCards = [];
+const iconColors = ['#e74c3c', '#f1c40f', '#3498db', '#9b59b6', '#2ecc71', '#e67e22', '#1abc9c', '#ff4757'];
+
+resetBtn.addEventListener("click", resetGame);
