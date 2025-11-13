cb4496784d883e3a743e097bff362a5a7586ee75
Annabelle Conron <annabelle.conron@outlook.com>
2025-11-13 20:40:29 +0100

    Declare constants

---
+++ Projects/Project-02/assets/script.js
@@ -0,0 +1,9 @@
+// list of font awsome icons for the game
+const icons = [
+    "fa-heart", "fa-heart", "fa-start", "fa-start", "fa-moon", "fa-moon", "fa-bell", "fa-bell", "fa-car", "fa-car", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-smile", "fa-smile"
+];
+
+const board = document.getElementById("game-board");
+const status = ocument.getElementById("status");
+const resetBtn = ocument.getElementById("reset-btn");
+
